7853a9d728457e47a31367ecda2aeb6dd067c8fe
sgs <sgs@HN-SKY-Z6099.local>
2022-06-09 16:39:02 +0700

    add module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
+import { MessagesModule } from './messages/messages.module';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(MessagesModule);
   await app.listen(3000);
 }
 bootstrap();
